00e26a515b6074687ce13ddcba29b67bbe15a537
NataDel <delynat@mail.ru>
2018-04-24 20:30:30 +0300

    My first Project

---
+++ lesson3/src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,13 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+    int total = sum(1, 2);
+        System.out.print("My First java application" + total);
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
