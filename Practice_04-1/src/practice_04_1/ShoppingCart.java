80feb3ce1616db1811087ba320d7c169a4fbf1ca
NataDel <delynat@mail.ru>
2018-04-26 19:42:28 +0300

    "My ShopingCart Project"

---
+++ Practice_04-1/src/practice_04_1/ShoppingCart.java
@@ -0,0 +1,15 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_04_1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName="Mary Smith", itemDesc="Shirt";
+        String message;
+        // Assign the message variable 
+        message=custName+"vans to purchase a "+itemDesc;
+        // Print and run the code
+System.out.println(message);
+    }
+}
